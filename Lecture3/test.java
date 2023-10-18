e7cd79d8dc4ff31201f9ec0afbb4bcf0afb5839a
Marin5358 <148369177+Marin5358@users.noreply.github.com>
2023-10-18 18:32:09 +0200

    Create test.java

---
+++ Lecture3/test.java
@@ -0,0 +1,17 @@
+
+public class exercise1 {
+	private int num;
+	public inr num2;
+	public myNumber() {
+		num=0;
+		num2=100;
+	}
+	int getNum() {
+		return num;
+	}
+	void setNum(int i) {
+		num=i;
+		num2=10*i;
+	}
+
+}
